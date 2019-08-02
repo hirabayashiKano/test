ae59b4c23317c5466a3362ef318c68fd7a9bad23
hirabayashiKano <52780060+hirabayashiKano@users.noreply.github.com>
2019-08-02 15:05:24 +0900

    employeedate編集

--- employee/src/employee/EmployeeData.java
+++ employee/src/employee/EmployeeData.java
@@ -1,5 +1,46 @@
 package employee;
 
+import java.time.LocalDate;
+
 public class EmployeeData {
+	private int no; 				//no
+	private String empld; 			// 従業員ID
+	private String empName; 		//氏名
+	private LocalDate empBirthday; 	//生年月日
+
+	//noのgetter,setter
+	public int getNo() {
+		return no;
+	}
+
+	public void setNo(int no) {
+		this.no = no;
+	}
+
+	//従業員IDのgetter,setter
+	public String getEmpld() {
+		return empld;
+	}
+
+	public void setEmpld(String empld) {
+		this.empld = empld;
+	}
+
+	//氏名のgetter,setter
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	//生年月日のgetter,setter
+	public LocalDate getEmpBirthday() {
+		return empBirthday;
+	}
+	public void setEmpBirthday(LocalDate empBirthday) {
+		this.empBirthday = empBirthday;
+	}
 
 }
