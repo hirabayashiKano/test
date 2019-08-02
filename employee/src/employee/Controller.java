8c5d6b35cf4a79b328ff9b7aa5642b0f508a6f19
futa2794 <53158440+futa2794@users.noreply.github.com>
2019-08-02 15:37:41 +0900

    Controllerクラス、ControllerBaseクラス

--- employee/src/employee/Controller.java
+++ employee/src/employee/Controller.java
@@ -1,5 +1,80 @@
 package employee;
 
-public class Controller {
+class Controller implements ControllerBase {
+
+	//mainメソッド
+	public void main() {
+
+		boolean flag = true;
+		while (flag) {
+
+			//printInputMenuメソッドの呼び出し
+			VIEW.printInputMenu();
+			/*
+			 * 1.全従業員表示  2.各従業員表示  3.従業員登録
+			 *
+			 * 4.従業員修正  5.従業員削除  0.終了
+			 *
+			 */
+			switch(prinInputMenu()) {
+			case 1 :
+				selectAllEmpData();
+				break;
+
+			case 2 :
+				selectOneEmpData();
+				break;
+
+			case 3 :
+				insertOneEmpData();
+				break;
+
+			case 4 :
+				updateOneEmpData();
+				break;
+
+			case 5 :
+				deleteOneEmpData();
+				break;
+
+			case 0 :
+				flag = false;
+
+				break;
+			}
+
+		}
+
+		//selectAllEmpDataメソッド(オーバーライド)
+		@Override
+		public abstract void selectAllEmpData() {
+
+		};
+
+		//selectOneEmpDataメソッド(オーバーライド)
+		@Override
+		public abstract void selectOneEmpData() {
+
+		};
+
+		//insertOneEmpDataメソッド(オーバーライド)
+		@Override
+		public abstract void insertOneEmpData() {
+
+		};
+
+		//updateOneEmpDataメソッド(オーバーライド)
+		@Override
+		public abstract void updateOneEmpData() {
+
+		};
+
+		//deleteOneEmpDataメソッド（オーバーライド）：結果を表示させる
+		@Override
+		public abstract void deleteOneEmpData() {
+
+		};
+
+	}
 
 }
--- employee/src/employee/ControllerBase.java
+++ employee/src/employee/ControllerBase.java
@@ -1,5 +1,20 @@
 package employee;
 
-public interface ControllerBase {
+interface ControllerBase {
+	VIEW=new View();
+	DAO=new Dao();
 
+	public abstract void selectAllEmpData() ;
+
+	//selectOneEmpDataメソッド(オーバーライド)
+	public abstract void selectOneEmpData() ;
+
+	//insertOneEmpDataメソッド(オーバーライド)
+	public abstract void insertOneEmpData() ;
+
+	//updateOneEmpDataメソッド(オーバーライド)
+	public abstract void updateOneEmpData() ;
+
+	//deleteOneEmpDataメソッド（オーバーライド）：結果を表示させる
+	public abstract void deleteOneEmpData() ;
 }
